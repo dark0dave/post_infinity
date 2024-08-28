ef91554d35ccbfa25ff61e94ef3bc21e26f1c810
dark0dave <dark0dave@mykolab.com>
2024-08-28 17:27:35 +0100

    feat(cre): Better cre parser
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- models/src/creature.rs
+++ models/src/creature.rs
@@ -4,10 +4,10 @@ use binrw::{
 };
 use serde::{Deserialize, Serialize};
 
-use crate::common::char_array::CharArray;
 use crate::common::{resref::Resref, strref::Strref};
 use crate::effect_v1::EffectV1;
 use crate::item_table::ItemReferenceTable;
+use crate::{common::char_array::CharArray, effect_v2::EffectV2Body};
 use crate::{
     item_table::ItemSlots,
     model::Model,
@@ -26,8 +26,10 @@ pub struct Creature {
     pub memorized_spell_info: Vec<SpellMemorizationInfo>,
     #[br(count=header.count_of_memorized_spell_table, seek_before=SeekFrom::Start(header.offset_to_memorized_spell_table as u64))]
     pub memorized_spells: Vec<SpellMemorizationTable>,
-    #[br(count=header.count_of_effects, seek_before=SeekFrom::Start(header.offset_to_effects as u64))]
-    pub effects: Vec<EffectV1>,
+    #[br(if(header.effstructure == 0), count=header.count_of_effects, seek_before=SeekFrom::Start(header.offset_to_effects as u64))]
+    pub effects_v1: Vec<EffectV1>,
+    #[br(if(header.effstructure == 1), count=header.count_of_effects, seek_before=SeekFrom::Start(header.offset_to_effects as u64))]
+    pub effects_v2: Vec<EffectV2Body>,
     #[br(count=header.count_of_items, seek_before=SeekFrom::Start(header.offset_to_items as u64))]
     pub item_table: Vec<ItemReferenceTable>,
     #[br(seek_before=SeekFrom::Start(header.offset_to_item_slots as u64))]
